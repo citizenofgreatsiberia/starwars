e3458df44173a8801828ef5c054988ff2a17d3a4
citizenofgreatsiberia <citizenofgreatsiberian@gmail.com>
2022-01-31 13:42:07 +0800

    add project to main

---
+++ src/utils/network.js
@@ -0,0 +1,50 @@
+import { HTTP, HTTPS } from '@constants/api';
+
+/**
+ * Изменяет url с HTTP на https
+ * @param {String} url - url для изменения
+ * @returns {String} - url  с https
+ */
+
+export const changeHTTP = url => {
+    const result = url ? url.replace('HTTP, HTTPS') : url;
+
+    return result;
+}
+
+/**
+ * Отправляет запрос fetch
+ * @param {String} url - url для запроса
+ * @returns {Promise} - Promise с результатом запроса
+ */
+
+export const getApiResource = async (url) => {
+    try {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            console.error('Could not fetch.', res.status);
+            return false;
+        }
+
+        return await res.json();
+    } catch (error) {
+        console.error('Could not fetch.', error.message);
+        return false;
+    }
+}
+
+// getApiResource(SWAPI_ROOT + SWAPI_PEOPLE)
+//     .then(body => console.log(body))
+
+// (async () => {
+//     const body = await getApiResource(SWAPI_ROOT + SWAPI_PEOPLE);
+//     console.log(body);
+// })();
+
+export const makeConcurrentRequest = async (url) => {
+    const res = await Promise.all(url.map(res => {
+        return fetch(res).then(res => res.json())
+    }));
+    return res;
+}
